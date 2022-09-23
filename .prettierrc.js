9dbe574135c17fab0066a0478b81607cc000ea71
Pengfei Gao <gpfdeveloper@gmail.com>
2022-09-23 19:20:08 +0800

    init project with ESLint

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+//Detail configuration: https://prettier.io/docs/en/options.html
+
+module.exports = {
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	quoteProps: "as-needed",
+	singleQuote: true,
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: false,
+	bracketSameLine: false,
+	arrowParens: "always",
+};
